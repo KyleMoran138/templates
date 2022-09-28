50f19ea4648f60d43b0353110514dc5cfc52a24c
KyleMoran138 <kylemoran.data@gmail.com>
2022-09-28 16:57:29 -0400

    fix linting thing and backend url for client

--- fullstack/nestjs-react-prisma/client/src/store.ts
+++ fullstack/nestjs-react-prisma/client/src/store.ts
@@ -1,9 +1,15 @@
 import create from 'zustand';
 import { CountSlice, createCountSlice } from './slices/count.slice';
 
-export type Store = {}
+export type Store = {
+  BACKEND_URL: string;
+}
 & CountSlice;
 
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL ? `${import.meta.env.VITE_BACKEND_URL}/api` : `http://${window.location.hostname}/api`;
+
+console.log('backend url', BACKEND_URL, import.meta.env.VITE_BACKEND_URL)
 export const useStore = create<Store>()((...a) => ({
+  BACKEND_URL,
   ...createCountSlice(...a),
 }));
